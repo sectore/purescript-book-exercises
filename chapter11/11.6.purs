cc5fad25afa4710e3123d125755a4f5a58b133c6
sectore <info@websector.de>
2016-11-14 13:14:56 +0100

    exercises of 11.6

---
+++ chapter11/11.6.purs
@@ -0,0 +1,34 @@
+
+-- -------------------------------------------------
+-- ex. 11.6.1
+-- -------------------------------------------------
+
+import Prelude (Unit, ($))
+import Data.Foldable (traverse_)
+import Control.Monad.Writer (Writer, tell, runWriter)
+import Data.Monoid.Additive (Additive(Additive))
+import Data.Tuple (Tuple)
+
+sumArrayW :: Array Int -> Writer (Additive Int) Unit
+sumArrayW = traverse_ \n -> tell $ Additive n
+
+runSumArrayW :: Array Int -> Tuple Unit (Additive Int)
+runSumArrayW = runWriter <<< sumArrayW
+
+
+-- -------------------------------------------------
+-- ex. 11.6.2
+-- -------------------------------------------------
+
+collatz :: Int -> Array Int
+collatz 1 = [1]
+collatz n	| odd n 		= cons n $ collatz (3 * n + 1)
+					| otherwise	= cons n $ collatz (n / 2)
+
+collatzW :: Int -> Writer (Array Int) Unit
+collatzW 1 = tell [1]
+collatzW n	| odd n 		= tell $ cons n $ collatz (3 * n + 1)
+						| otherwise	= tell $ cons n $ collatz (n / 2)
+
+runCollatzW :: Int -> Tuple Unit (Array Int)
+runCollatzW = runWriter <<< collatzW
--- chapter11/README.md
+++ chapter11/README.md
@@ -29,3 +29,20 @@ which tests whether or not a `String` of parentheses is balanced, by keeping tra
 ```
 
 _Hint:_ you may like to use the `toCharArray` function from the `Data.String` module to turn the input string into an array of characters.
+
+## [11.6.](./11.6.purs))
+
+
+1. (Medium) Rewrite the `sumArray` function above using the `Writer` monad and the `Additive Int` monoid from the `purescript-monoid` package.
+
+2. (Medium) The _Collatz_ function is defined on natural numbers `n` as `n / 2` when `n` is even, and `3 * n + 1` when `n` is odd. For example, the iterated Collatz sequence starting at `10` is as follows:
+
+```purescript
+10, 5, 16, 8, 4, 2, 1, ...
+```
+
+It is conjectured that the iterated Collatz sequence always reaches `1` after some finite number of applications of the Collatz function.
+
+Write a function which uses recursion to calculate how many iterations of the Collatz function are required before the sequence reaches `1`.
+
+Modify your function to use the `Writer` monad to log each application of the Collatz function.
